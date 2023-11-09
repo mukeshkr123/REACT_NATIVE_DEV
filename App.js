b38b849b3bc4e9cfb406c5eb0bd4a590af9414b8
mukesh kumar <mkmehta2041@gmail.com>
2023-11-09 11:26:34 +0530

    Building the Welcome Screen

--- App.js
+++ App.js
@@ -1,54 +1,5 @@
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", //horizontal
-        justifyContent: "center", // main
-        alignItems: "center", // secondary
-        flexWrap: "wrap",
-        alignContent: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
